14438a16522f08d8843665b3a4a347f480271b9d
Samet <samet.okumus@codbt.com>
2022-10-09 16:25:09 +0300

    product-campaign

---
+++ database/migrations/2022_10_09_132243_create_campaign_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampaignProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaign_products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id');
+            $table->tinyInteger('order')->default(1);
+            $table->tinyInteger('active')->default(1);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaign_products');
+    }
+}
