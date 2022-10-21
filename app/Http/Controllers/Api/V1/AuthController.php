ee6e9b20cd839972feb3632031ddbd2a571ab493
Samet <samet.okumus@codbt.com>
2022-10-21 03:39:44 +0300

    test

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -101,7 +101,7 @@ class AuthController extends Controller
 
             $user->token = $userToken;
 
-            return  response(['message' => 'Başarılı.','status' => 'success', 'object' => ['user'=>$user]]);
+            return  response(['message' => 'Başarılıs.','status' => 'success', 'object' => ['user'=>$user]]);
         } catch (ValidationException $validationException) {
             return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
         } catch (QueryException $queryException) {
