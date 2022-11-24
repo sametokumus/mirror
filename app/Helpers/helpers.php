afc756fae65fe95f872e84f0452f418205a04ffa
Samet <samet.okumus@codbt.com>
2022-11-24 11:19:45 +0300

    currency:getAllProductWithVariationById

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -6,9 +6,10 @@ use Carbon\Carbon;
 if (! function_exists('convertUSDtoTRY')) {
     function convertUSDtoTRY($price)
     {
-        $today = date('Y-m-d');
-        $yesterday = date('Y-m-d',strtotime($today)-1);
-        $currency = CurrencyLog::query()->where('day', $yesterday)->first()->dollar;
+//        $today = date('Y-m-d');
+//        $yesterday = date('Y-m-d',strtotime($today)-1);
+//        $currency = CurrencyLog::query()->where('day', $yesterday)->first()->dollar;
+        $currency = CurrencyLog::query()->orderBy('id', 'desc')->first()->dollar;
         return number_format($price * $currency, 2,".","");
     }
 }
@@ -16,9 +17,10 @@ if (! function_exists('convertUSDtoTRY')) {
 if (! function_exists('convertEURtoTRY')) {
     function convertEURtoTRY($price)
     {
-        $today = date('Y-m-d');
-        $yesterday = date('Y-m-d',strtotime($today)-1);
-        $currency = CurrencyLog::query()->where('day', $yesterday)->first()->euro;
+//        $today = date('Y-m-d');
+//        $yesterday = date('Y-m-d',strtotime($today)-1);
+//        $currency = CurrencyLog::query()->where('day', $yesterday)->first()->euro;
+        $currency = CurrencyLog::query()->orderBy('id', 'desc')->first()->euro;
         return number_format($price * $currency, 2,".","");
     }
 }
