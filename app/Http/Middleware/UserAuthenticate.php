085146c75a1f213636fb39c63d5d8e62af0c17cd
Samet <samet.okumus@codbt.com>
2023-05-30 12:06:40 +0300

    update all status

--- app/Http/Middleware/UserAuthenticate.php
+++ app/Http/Middleware/UserAuthenticate.php
@@ -2,43 +2,21 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-class UserAuthenticate
+class UserAuthenticate extends Middleware
 {
     /**
-     * Handle an incoming request.
+     * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
+     * @return string|null
      */
-    public function handle($request, Closure $next)
+    protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
+//            return route('auth.login');
         }
-
-        return $next($request);
     }
 }
-
-//namespace App\Http\Middleware;
-//
-//use Illuminate\Auth\Middleware\Authenticate as Middleware;
-//
-//class UserAuthenticate extends Middleware
-//{
-//    /**
-//     * Get the path the user should be redirected to when they are not authenticated.
-//     *
-//     * @param  \Illuminate\Http\Request  $request
-//     * @return string|null
-//     */
-//    protected function redirectTo($request)
-//    {
-//        if (! $request->expectsJson()) {
-//            return route('auth.login');
-//        }
-//    }
-//}
