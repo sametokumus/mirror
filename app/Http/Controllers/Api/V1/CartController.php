887832446acd0779316a803d1d2bff23f4ea9710
Samet <samet.okumus@codbt.com>
2023-06-10 11:09:33 +0300

    update all status

--- app/Http/Controllers/Api/V1/CartController.php
+++ app/Http/Controllers/Api/V1/CartController.php
@@ -485,8 +485,8 @@ class CartController extends Controller
                     }
                 }
                 if ($product->is_free_shipping == 0) {
-                    $weight = $weight + ($cart_detail->quantity / $rule->quantity_step * $rule->weight);
-                    $material_array[$rule->material] = $material_array[$rule->material] + ($cart_detail->quantity / $rule->quantity_step * $rule->weight);
+                    $weight = $weight + ($cart_detail->quantity * $rule->weight);
+                    $material_array[$rule->material] = $material_array[$rule->material] + ($cart_detail->quantity * $rule->weight);
                 }else{
                     $weight = $weight + 0;
                     $material_array[$rule->material] = $material_array[$rule->material] + 0;
