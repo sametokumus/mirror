856c59523995cf0ce8d2abd947fbd9051a97a476
Samet <samet.okumus@codbt.com>
2024-05-16 13:21:16 +0300

    update bank request

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,17 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('user_name')->nullable();
-            $table->string('email')->unique();
-            $table->string('phone_number')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->unique()->nullable();
             $table->string('password');
             $table->string('token')->unique()->nullable();
             $table->rememberToken();
             $table->boolean('active')->default(false);
-            $table->boolean('verified')->default(false);
-            $table->boolean('user_type')->default(1);
-            $table->boolean('user_group')->default(false);
-            $table->decimal('user_discount')->nullable()->default(0);
+            $table->boolean('email_verified')->default(false);
+            $table->boolean('sms_verified')->default(false);
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('sms_verified_at')->nullable();
             $table->timestamp('deleted_at')->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
