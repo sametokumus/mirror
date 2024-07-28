3f9775da99d00147732f30bf97e5d6e11ac250a4
Samet <samet.okumus@codbt.com>
2024-07-28 12:19:06 +0300

    commt x

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -57,7 +57,7 @@ class AuthController extends Controller
             //Oluşturulan Kullanıcıyı mail yolluyor
             $user->sendApiConfirmAccount($user);
 
-            return response(['message' => 'Kullanıcı başarıyla oluşturuldu sisteme giriş için epostanızı kontrol ediniz.','status' => 'success']);
+            return response(['message' => 'Kullanıcı başarıyla oluşturuldu sisteme giriş için epostanızı kontrol ediniz.','status' => 'success', 'token' => $userToken]);
         } catch (ValidationException $validationException) {
             return  response(['message' => 'Lütfen girdiğiniz bilgileri kontrol ediniz.','status' => 'validation-001']);
         } catch (QueryException $queryException) {
