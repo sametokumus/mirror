4cd9ea698ee71efcfd2f9d3d9eec108e86d2d0ca
Samet <samet.okumus@codbt.com>
2024-11-08 20:15:58 +0300

    commt x

--- routes/api_admin.php
+++ routes/api_admin.php
@@ -331,12 +331,12 @@ Route::middleware(['auth:sanctum', 'type.admin'])->group(function (){
 
     Route::get('question/getQuestions', [QuestionController::class, 'getQuestions']);
     Route::post('question/addQuestion', [QuestionController::class, 'addQuestion']);
+    Route::get('question/getFilterQuestions', [QuestionController::class, 'getFilterQuestions']);
 
     Route::get('question/getScreens', [QuestionController::class, 'getScreens']);
     Route::get('question/getScreenById/{screen_id}', [QuestionController::class, 'getScreenById']);
     Route::post('question/addScreen', [QuestionController::class, 'addScreen']);
     Route::post('question/updateScreen', [QuestionController::class, 'updateScreen']);
-    Route::get('question/getQuestions', [QuestionController::class, 'getQuestions']);
 
 });
 
