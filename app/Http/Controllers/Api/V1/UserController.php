b9f9ec92a7ad2d96163b0986bb97173f7c541d14
Samet <samet.okumus@codbt.com>
2024-11-16 04:51:51 +0300

    commt x

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -13,6 +13,7 @@ use App\Models\UserFavorite;
 use App\Models\UserProfile;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
