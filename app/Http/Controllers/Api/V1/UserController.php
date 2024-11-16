49af64562c4d636ee0daa7efd9097619c3c323d5
Samet <samet.okumus@codbt.com>
2024-11-16 04:52:36 +0300

    commt x

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Nette\Schema\ValidationException;
 
