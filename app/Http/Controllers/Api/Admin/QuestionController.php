eb9dfb2cfee62cc544830c117c899c8a78ee6d17
Samet <samet.okumus@codbt.com>
2024-11-24 17:25:13 +0300

    commt x

--- app/Http/Controllers/Api/Admin/QuestionController.php
+++ app/Http/Controllers/Api/Admin/QuestionController.php
@@ -113,7 +113,7 @@ class QuestionController extends Controller
     public function getScreens()
     {
         try {
-            $screens = Screen::query()->get();
+            $screens = Screen::with('questions.options')->get();
             return response(['message' => 'İşlem Başarılı.', 'status' => 'success', 'object' => ['screens' => $screens]]);
         } catch (QueryException $queryException) {
             return response(['message' => 'Hatalı sorgu.', 'status' => 'query-001']);
